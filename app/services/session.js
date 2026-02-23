20ff6f2a2ba4c8d188266e9ac57561a8f4ca3721
Niels Steenbergen <niels.steenbergen@triply.cc>
2026-02-23 17:48:35 +0100

    Add buy-access button.

--- app/services/session.js
+++ app/services/session.js
@@ -3,6 +3,7 @@ import { service } from '@ember/service';
 
 export default class DecideSessionService extends SessionService {
   @service currentSession;
+  @service router;
 
   get isMockLoginSession() {
     return this.isAuthenticated
@@ -12,7 +13,20 @@ export default class DecideSessionService extends SessionService {
 
   async handleAuthentication(routeAfterAuthentication) {
     await this.currentSession.load();
-    super.handleAuthentication(routeAfterAuthentication);
+
+    // (Workaround:) When we log in through the buy access button, we
+    // arrive in a logged in state as expected but we also immediately
+    // want to present a QR code for receiving a verifiable credential.
+    if (this.router.currentRouteName === 'auth.buy-access'){
+      // ember-simple-auth wants to bring us back to the route that we
+      // started with before authentication, but in this case we don't
+      // want that
+      this.attemptedTransition = null;
+      super.handleAuthentication('auth.receive-credential');
+    }
+    else {
+      super.handleAuthentication(routeAfterAuthentication);
+    }
   }
 
   async handleInvalidation() {
